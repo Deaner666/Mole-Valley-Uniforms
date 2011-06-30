dc1f5371165173e13986b6d8729d6bdd7fb1d863
Dave Dean <mail@dave-dean.co.uk>
2011-06-30 18:01:16 +0100

    Changed Brandon Hire to Mole Valley Farmers in titles and in the layout.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   # Return a title on a per-page basis.
   def global_title
-    base_title = "Brandon Hire Work Wear"
+    base_title = "Mole Valley Farmers Uniforms"
     @title ? base_title + " | " + @title : base_title
   end
   
