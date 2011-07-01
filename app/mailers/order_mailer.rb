0f5803bd5e48600505eadd9ee639489e6cf8f9dd
Dave Dean <mail@dave-dean.co.uk>
2011-07-01 20:03:12 +0100

    Updates to change Brandon Hire to MVF in mailers and mailer views.

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,10 +3,10 @@ class OrderMailer < ActionMailer::Base
   def order_email(order)
     @order = order
     mail(:from => "sales@monmark.co.uk",
-         :to => "stationery@brandonhire.co.uk",
+         :to => "mail@dave-dean.co.uk",
          :cc => "sales@monmark.co.uk",
          :bcc => "mail@dave-dean.co.uk",
-         :subject => "An order for uniforms has been placed by a Brandon Hire branch")
+         :subject => "An order for uniforms has been placed by a Mole Valley Farmers branch")
   end
   
 end
